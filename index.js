c78c5e5c22e1cbb595a2eafdeabe56e8179ba53d
Roshan <155551955+okroshan4u@users.noreply.github.com>
2026-02-06 23:01:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(cors({
   credentials: true,
 }));
 
-// mongodb connecti
+// mongodb connection
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.error('MongoDB connection error:', err));
